eacfca1612a669bbee734c16e45272e5d7b773d0
Tshaka Lekholoane <23554017+ketshaka@users.noreply.github.com>
2019-04-08 20:47:05 +0200

    Initialise Guided Project - Light

---
+++ 1 Getting Started/Light/Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Tshaka Lekholoane on 08/04/2019.
+//  Copyright © 2019 Tshaka Lekholoane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
