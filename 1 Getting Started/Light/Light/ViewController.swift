4010691a663e915a2f64e266e307e117d6ca9431
Tshaka Lekholoane <23554017+ketshaka@users.noreply.github.com>
2019-04-10 20:27:39 +0200

    4 Improve the user experience
    - Enlarge button to fill the screen
    - Remove button text
    - Replace if-else statement with ternary statement

--- 1 Getting Started/Light/Light/ViewController.swift
+++ 1 Getting Started/Light/Light/ViewController.swift
@@ -12,8 +12,6 @@ class ViewController: UIViewController {
     
     var lightOn = true
     
-    @IBOutlet weak var lightButton: UIButton!
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -26,13 +24,7 @@ class ViewController: UIViewController {
     }
     
     func updateUI() {
-        if lightOn {
-            view.backgroundColor = .white
-            lightButton.setTitle("Off", for: .normal)
-        } else {
-            view.backgroundColor = .black
-            lightButton.setTitle("On", for: .normal)
-        }
+        view.backgroundColor = lightOn ? .white : .black
     }
 
 }
