dfd4e2e73aa93bbeeeac629d793d14f019bbc9e2
Tshaka Lekholoane <23554017+ketshaka@users.noreply.github.com>
2019-05-14 20:16:34 +0200

    Add Part Five - Handle a Win or Loss

--- 2 Introduction to UIKit/Apple Pie/Apple Pie/ViewController.swift
+++ 2 Introduction to UIKit/Apple Pie/Apple Pie/ViewController.swift
@@ -18,8 +18,16 @@ class ViewController: UIViewController {
     var listOfWords = ["pirate", "fast", "magnificent", "angry", "insect", "list"]
     let incorrectMovesAllowed = 7
     
-    var totalWins = 0
-    var totalLosses = 0
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,9 +38,20 @@ class ViewController: UIViewController {
     var currentGame: Game!
     
     func newRound() {
-        let newWord = listOfWords.removeFirst()
-        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
-        updateUI()
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        } else {
+            enableLetterButtons(false)
+        }
+    }
+    
+    func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
@@ -40,7 +59,17 @@ class ViewController: UIViewController {
         let letterString = sender.title(for: .normal)!
         let letter = Character(letterString.lowercased())
         currentGame.playerGuessed(letter: letter)
-        updateUI()
+        updateGameState()
+    }
+    
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
     }
     
     func updateUI() {
