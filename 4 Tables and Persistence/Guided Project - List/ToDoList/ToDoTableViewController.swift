108ad35349796e4311a5339a2ffea52f81450bd3
Tshaka Lekholoane <23554017+ketshaka@users.noreply.github.com>
2019-06-16 16:59:06 +0200

    Update Guided Project - List
    Add and delete controls

--- 4 Tables and Persistence/Guided Project - List/ToDoList/ToDoTableViewController.swift
+++ 4 Tables and Persistence/Guided Project - List/ToDoList/ToDoTableViewController.swift
@@ -20,6 +20,8 @@ class ToDoTableViewController: UITableViewController {
         } else {
             todos = ToDo.loadSampleToDos()
         }
+        
+        navigationItem.leftBarButtonItem = editButtonItem
     }
 
     // MARK: - Table view data source
@@ -33,7 +35,6 @@ class ToDoTableViewController: UITableViewController {
         return todos.count
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCellIdentifier", for: indexPath)
 
@@ -41,27 +42,20 @@ class ToDoTableViewController: UITableViewController {
         cell.textLabel?.text = todo.title
         return cell
     }
-    
-
-    /*
+   
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            todos.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
