7f291e403d79ca6dd6957ece3fac3855ceafd695
Tshaka Lekholoane <23554017+ketshaka@users.noreply.github.com>
2019-06-17 13:26:02 +0200

    Update Guided Project - List
    Connect the static table view to code

---
+++ 4 Tables and Persistence/Guided Project - List/ToDoList/ToDoDetailTableViewController.swift
@@ -0,0 +1,151 @@
+//
+//  ToDoDetailTableViewController.swift
+//  ToDoList
+//
+//  Created by Tshaka Lekholoane on 17/06/2019.
+//  Copyright © 2019 Tshaka Lekholoane. All rights reserved.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var isCompleteButton: UIButton!
+    @IBOutlet weak var dueDateLabel: UILabel!
+    @IBOutlet weak var dueDatePickerView: UIDatePicker!
+    @IBOutlet weak var notesTextView: UITextView!
+    
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    var isPickerHidden = true
+    
+    let dateLabelIndexPath = IndexPath(row: 0, section: 1)
+    let datePickerIndexPath = IndexPath(row: 1, section: 1)
+    let notesTextViewIndexPath = IndexPath(row: 0, section: 2)
+    
+    let normalCellHeight: CGFloat = 44
+    let largeCellHeight: CGFloat = 200
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dueDatePickerView.date = Date().addingTimeInterval(24 * 60 * 60)
+        updateDueDateLabel(date: dueDatePickerView.date)
+        updateSaveButtonState()
+    }
+
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case datePickerIndexPath:
+            return isPickerHidden ? 0 : dueDatePickerView.frame.height
+        case notesTextViewIndexPath:
+            return largeCellHeight
+        default:
+            return normalCellHeight
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath == dateLabelIndexPath {
+            isPickerHidden = !isPickerHidden
+            dueDateLabel.textColor = isPickerHidden ? .black : tableView.tintColor
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }
+    }
+    
+    @IBAction func textEditingChanged(_ sender: UITextField) {
+        updateSaveButtonState()
+    }
+    
+    @IBAction func returnPressed(_ sender: UITextField) {
+        titleTextField.resignFirstResponder()
+    }
+    
+    @IBAction func isCompleteButtonTapped(_ sender: UIButton) {
+        isCompleteButton.isSelected = !isCompleteButton.isSelected
+    }
+    
+    @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+        updateDueDateLabel(date: dueDatePickerView.date)
+    }
+    
+    // Helper methods
+    
+    func updateSaveButtonState() {
+        let text = titleTextField.text ?? ""
+        saveButton.isEnabled = !text.isEmpty
+    }
+    
+    func updateDueDateLabel(date: Date) {
+        dueDateLabel.text = ToDo.dueDateFormatter.string(from: date)
+    }
+
+}
