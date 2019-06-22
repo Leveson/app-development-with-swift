ed37b929194463de81995281113771cc4ac2f743
Tshaka Lekholoane <23554017+ketshaka@users.noreply.github.com>
2019-06-22 10:32:25 +0200

    Update Lab - iTunes Search (Part 3)
    Import previous playground code

---
+++ 5 Working with the Web/6 Concurrency/Lab - iTunes Search (Part 3)/iTunesSearch/StoreItem.swift
@@ -0,0 +1,35 @@
+//
+//  StoreItem.swift
+//  iTunesSearch
+//
+//  Created by Tshaka Lekholoane on 22/06/2019.
+//  Copyright © 2019 Caleb Hicks. All rights reserved.
+//
+
+import Foundation
+
+struct StoreItems: Codable {
+    let results: [StoreItem]
+}
+
+struct StoreItem: Codable {
+    var artist: String
+    var name: String
+    var genre: String
+    var rating: String
+    
+    enum CodingKeys: String, CodingKey {
+        case artist = "artistName"
+        case name = "trackName"
+        case genre = "primaryGenreName"
+        case rating = "contentAdvisoryRating"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        name = try values.decode(String.self, forKey: CodingKeys.name)
+        artist = try values.decode(String.self, forKey: CodingKeys.artist)
+        genre = try values.decode(String.self, forKey: CodingKeys.genre)
+        rating = try values.decode(String.self, forKey: CodingKeys.rating)
+    }
+}
